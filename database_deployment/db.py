6eeb99a6166909628e27e905f040032d789a66a5
kora11e <87006301+kora11e@users.noreply.github.com>
2023-12-20 21:43:14 +0100

    update

--- database_deployment/db.py
+++ database_deployment/db.py
@@ -4,40 +4,35 @@ from chromadb.utils import embedding_functions
 import csv
 
 
-sentence_transformer_ef = embedding_functions.SentenceTransformerEmbeddingFunction(model_name='all-MiniLM-L6-v2')
-
-
-with open('item.csv') as file:
-    lines = csv.reader(file)
-
-    documents = []
-    metadatas = []
-    ids = []
-    id = 1
-
-    for i, line in enumerate(lines):
-        if i == 0:
-            continue
-        
-        documents.append(line[1])
-        metadatas.append({'item_id': line[0]})
-        ids.append(str(id))
-        id += 1
-
 chroma_client = chromadb.Client()
 
-collection = chroma_client.create_collection(name='sample_1', embedding_function=sentence_transformer_ef)
+collection_cS = chroma_client.create_collection(name='colletion_cS')
+collection_lc = chroma_client.create_collection(name='colletion_lc')
 
-collection.add(
+collection_cS.add(
     documents = ['Boilerplate document', 'Boilerplate document 2'],
     metadatas = [{'source': 'my_source'}, {'source_2','my_source_2'}],
     ids = ['id1', 'id2']
 )
 
-results = collection.query(
+collection_lc.add(
+    documents = ['Boilerplate document', 'Boilerplate document 2'],
+    metadatas = [{'source': 'my_source'}, {'source_2','my_source_2'}],
+    ids = ['id1', 'id2']
+)
+
+results_cS = collection_cS.query(
     query_texts = ['This is a query document'],
-    n_results = 1,
+    n_results = 10,
     include = ['distances', 'metadatas', 'embeddings', 'documents']
 )
 
-results
+print(results_cS)
+
+results_lc = collection_lc.query(
+    query_texts= ['This is a lc document'],
+    n_results= 10,
+    include = []
+)
+
+print(results_lc)
